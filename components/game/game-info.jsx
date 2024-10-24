6d63f64f72dabd306a14b1151414f4875d22f926
Ivan <99608223+ivan1945kozhevnikov@users.noreply.github.com>
2024-10-24 19:42:50 +0300

    Сверстал GameTitle и GameInfo

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -1,21 +1,32 @@
-import { GameSymbol } from "./game-symbol";
+import clsx from "clsx";
+import { Profile } from "../profile";
+import { CrossIcon } from "./icons/cross-icon";
 
-export function GameInfo({ isDrow, winnerSymbol, currentStep }) {
-  if (isDrow) {
-    return <div className="mb-2.5">Ничья</div>;
-  }
-
-  if (winnerSymbol) {
-    return (
-      <div className="mb-2.5">
-        Победитель: <GameSymbol symbol={winnerSymbol} />
-      </div>
-    );
-  }
-
+export function GameInfo({ className }) {
   return (
-    <div className="mb-2.5">
-      Ход: <GameSymbol symbol={currentStep} />
+    <div
+      className={clsx(className, "flex justify-between bg-white rounded-2xl shadow-md px-8 py-4")}
+    >
+      <div className="flex gap-3 items-center">
+        <div className="relative">
+          <Profile className="w-44" />
+          <div className="w-5 h-5 flex justify-center items-center rounded-full bg-white shadow absolute -left-1 -top-1">
+            <CrossIcon />
+          </div>
+        </div>{" "}
+        <div className="h-6 w-px bg-slate-200" />
+        <div className="text-slate-900 text-lg font-semibold">01:08</div>
+      </div>
+      <div className="flex gap-3 items-center">
+      <div className="text-orange-600 text-lg font-semibold">00:08</div>
+      <div className="h-6 w-px bg-slate-200" />
+        <div className="relative">
+          <Profile className="w-44" />
+          <div className="w-5 h-5 flex justify-center items-center rounded-full bg-white shadow absolute -left-1 -top-1">
+            <CrossIcon />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
