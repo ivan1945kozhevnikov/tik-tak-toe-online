e2564f0df19512f465ae54ff0ad4a921853b962e
Ivan <99608223+ivan1945kozhevnikov@users.noreply.github.com>
2024-10-26 15:12:09 +0300

    Переписал GameField используя новые компоненты

--- components/game/game-field.jsx
+++ components/game/game-field.jsx
@@ -6,6 +6,41 @@ import { UiButton } from "../uikit/ui-button";
 const cells = new Array(19 * 19).fill(null);
 
 export function GameField({ className }) {
+  const actions = (
+    <>
+      <UiButton size="md" variant="primary">
+        Ничья
+      </UiButton>
+      <UiButton size="md" variant="outline">
+        Сдаться
+      </UiButton>
+    </>
+  );
+
+  return (
+    <GameFieldLayout className={className}>
+      <GameMoveInfo acctions={actions} />
+      <GameGrid>
+        {cells.map((_, index) => {
+          <GameCell key={index}></GameCell>;
+        })}
+      </GameGrid>
+    </GameFieldLayout>
+  );
+}
+
+function GameCell({ children }) {
+  return (
+    <button
+      key={i}
+      className="border border-slate-200 -ml-px -mt-px flex items-center justify-center"
+    >
+      {children}
+    </button>
+  );
+}
+
+function GameFieldLayout({ children, className }) {
   return (
     <div
       className={clsx(
@@ -13,31 +48,31 @@ export function GameField({ className }) {
         "bg-white rounded-2xl shadow-md px-8 pt-5 pb-7",
       )}
     >
-      <div className="flex gap-3 items-center">
-        <div className="mr-auto">
-          <div className="flex items-center gap-1 text-xl leading-tight">
-            Ход: <ZeroIcon className="w-5 h-5" />
-          </div>
-          <div className="flex items-center gap-1 text-xs leading-tight text-slate-400">
-            Следующий: <CrossIcon />
-          </div>
-        </div>
-        <UiButton size="md" variant="primary">
-          Ничья
-        </UiButton>
-        <UiButton size="md" variant="outline">
-          Сдаться
-        </UiButton>
-      </div>
+      {children}
+    </div>
+  );
+}
 
-      <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pt-px pl-px mt-3">
-        {cells.map((_, i) => (
-          <button
-            key={i}
-            className="border border-slate-200 -ml-px -mt-px flex items-center justify-center"
-          ></button>
-        ))}
+function GameMoveInfo({ actions }) {
+  return (
+    <div className="flex gap-3 items-center">
+      <div className="mr-auto">
+        <div className="flex items-center gap-1 text-xl leading-tight">
+          Ход: <ZeroIcon className="w-5 h-5" />
+        </div>
+        <div className="flex items-center gap-1 text-xs leading-tight text-slate-400">
+          Следующий: <CrossIcon />
+        </div>
       </div>
+      {actions}
+    </div>
+  );
+}
+
+function GameGrid({ children }) {
+  return (
+    <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pt-px pl-px mt-3">
+      {children}
     </div>
   );
 }
