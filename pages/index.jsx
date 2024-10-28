0f921d37bcd1c757029dff8ae4c8864a73cbc29f
Ivan <99608223+ivan1945kozhevnikov@users.noreply.github.com>
2024-10-28 18:55:29 +0300

    Добавил таймер хода для пользователей

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
-import { GameField, GameInfo, GameTitle } from "../components/game";
+import {
+  GameField,
+  GameInfo,
+  GameTitle,
+  useGameState,
+} from "../components/game";
 import { Header } from "../components/header";
 
 export default function HomePage() {
   const [playersCount] = useState(4);
+  const { cells, currentMove, nextMove, handleCellClick } =
+    useGameState(playersCount);
+
   return (
     <div className="bg-slate-50 min-h-screen">
       <Header />
       <main className="pt-6 mx-auto w-max">
         <GameTitle playersCount={playersCount} />
-        <GameInfo playersCount={playersCount} className="mt-4" />
-        <GameField playersCount={playersCount} className="mt-6" />
+        <GameInfo
+          playersCount={playersCount}
+          currentMove={currentMove}
+          className="mt-4"
+        />
+        <GameField
+          cells={cells}
+          currentMove={currentMove}
+          nextMove={nextMove}
+          handleCellClick={handleCellClick}
+          className="mt-6"
+        />
       </main>
     </div>
   );
