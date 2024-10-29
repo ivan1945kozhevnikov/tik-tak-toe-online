65fffa945568515b93def3becc62f94e46b6cd6d
Ivan <99608223+ivan1945kozhevnikov@users.noreply.github.com>
2024-10-29 20:41:15 +0300

    Добавил победу по времени

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -39,7 +39,13 @@ const players = [
   },
 ];
 
-export function GameInfo({ className, playersCount, currentMove }) {
+export function GameInfo({
+  className,
+  playersCount,
+  currentMove,
+  isWinner,
+  onPlayerTimeOver,
+}) {
   return (
     <div
       className={clsx(
@@ -52,15 +58,18 @@ export function GameInfo({ className, playersCount, currentMove }) {
           key={player.id}
           playerInfo={player}
           isRight={index % 2 === 1}
-          isTimerRunning={currentMove === player.symbol}
+          onTimeOver={() => {
+            onPlayerTimeOver(player.symbol);
+          }}
+          isTimerRunning={currentMove === player.symbol && !isWinner}
         />
       ))}
     </div>
   );
 }
 
-function PlayerInfo({ playerInfo, isRight, isTimerRunning }) {
-  const [seconds, setSeconds] = useState(60);
+function PlayerInfo({ playerInfo, isRight, isTimerRunning, onTimeOver }) {
+  const [seconds, setSeconds] = useState(6);
 
   const minutesString = String(Math.floor(seconds / 60)).padStart(2, "0");
   const secondsString = String(seconds % 60).padStart(2, "0");
@@ -79,6 +88,13 @@ function PlayerInfo({ playerInfo, isRight, isTimerRunning }) {
       };
     }
   }, [isTimerRunning]);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      onTimeOver();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [seconds]);
 
   const getTimerColor = () => {
     if (isTimerRunning) {
@@ -105,7 +121,7 @@ function PlayerInfo({ playerInfo, isRight, isTimerRunning }) {
         className={clsx(
           "text-lg font-semibold w-[60px]",
           isRight && "order-1",
-          isDanger ? "text-orange-600" : getTimerColor(),
+          getTimerColor(),
         )}
       >
         {minutesString}:{secondsString}
--- components/game/use-game-state.js
+++ components/game/use-game-state.js
@@ -3,13 +3,19 @@ import { GAME_SIMBOLS } from "./constants";
 import { compuuteWinner, getNextMove } from "./model";
 
 export function useGameState(playersCount) {
-  const [{ cells, currentMove }, setGameState] = useState(() => ({
-    cells: new Array(19 * 19).fill(null),
-    currentMove: GAME_SIMBOLS.CROSS,
-  }));
+  const [{ cells, currentMove, playersTimeOver }, setGameState] = useState(
+    () => ({
+      cells: new Array(19 * 19).fill(null),
+      currentMove: GAME_SIMBOLS.CROSS,
+      playersTimeOver: [],
+    }),
+  );
 
   const winnerSequence = compuuteWinner(cells);
-  const nextMove = getNextMove(currentMove, playersCount);
+  const nextMove = getNextMove(currentMove, playersCount, playersTimeOver);
+
+  const winnerSymbol =
+    nextMove === currentMove ? currentMove : winnerSequence?.[0];
 
   const handleCellClick = (index) => {
     setGameState((lastGameState) => {
@@ -19,9 +25,27 @@ export function useGameState(playersCount) {
 
       return {
         ...lastGameState,
-        currentMove: getNextMove(lastGameState.currentMove, playersCount),
+        currentMove: getNextMove(
+          lastGameState.currentMove,
+          playersCount,
+          playersTimeOver,
+        ),
         cells: lastGameState.cells.map((cell, i) =>
           i === index ? lastGameState.currentMove : cell,
+        ),
+      };
+    });
+  };
+
+  const handlePlayerTimeOver = (symbol) => {
+    setGameState((lastGameState) => {
+      return {
+        ...lastGameState,
+        playersTimeOver: [...lastGameState.playersTimeOver, symbol],
+        currentMove: getNextMove(
+          lastGameState.currentMove,
+          playersCount,
+          lastGameState.playersTimeOver,
         ),
       };
     });
@@ -32,6 +56,8 @@ export function useGameState(playersCount) {
     currentMove,
     nextMove,
     handleCellClick,
+    handlePlayerTimeOver,
     winnerSequence,
+    winnerSymbol,
   };
 }
