adb726189bfef2927d532c65d1dfc43a52e532a4
Ivan <99608223+ivan1945kozhevnikov@users.noreply.github.com>
2024-11-02 17:15:52 +0300

    Сделал рефакторинг модального окна

--- components/game-new/game.jsx
+++ components/game-new/game.jsx
@@ -7,6 +7,7 @@ import { PlayerInfo } from "./ui/player-info";
 import { GameMoveInfo } from "./ui/game-move-info";
 import { useGameState } from "./model/use-game-state";
 import { GameCell } from "./ui/game-cell";
+import { GameOverModal } from "./ui/game-over-modal";
 
 const PLAYERS_COUNT = 2;
 
@@ -23,37 +24,53 @@ export function Game() {
   const winnerPlayer = PLAYERS.find((player) => player.symbol === winnerSymbol);
 
   return (
-    <GameLayout
-      backLink={<BackLink />}
-      title={<GameTitle />}
-      gameInfo={
-        <GameInfo isRatingGame playersCount={4} timeMode={"1 мин. на ход"} />
-      }
-      playersList={PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => (
-        <PlayerInfo
-          key={player.id}
-          avatar={player.avatar}
-          name={player.name}
-          rating={player.rating}
-          seconds={60}
-          symbol={player.symbol}
-          isRight={index % 2 === 1}
-        />
-      ))}
-      gameMoveInfo={
-        <GameMoveInfo currentMove={currentMove} nextMove={nextMove} />
-      }
-      gameCells={cells.map((cell, index) => (
-        <GameCell
-          key={index}
-          isWinner={winnerSequence?.includes(index)}
-          disabled={!!winnerSymbol}
-          onClick={() => {
-            handleCellClick(index);
-          }}
-          symbol={cell}
-        />
-      ))}
-    />
+    <>
+      <GameLayout
+        backLink={<BackLink />}
+        title={<GameTitle />}
+        gameInfo={
+          <GameInfo isRatingGame playersCount={4} timeMode={"1 мин. на ход"} />
+        }
+        playersList={PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => (
+          <PlayerInfo
+            key={player.id}
+            avatar={player.avatar}
+            name={player.name}
+            rating={player.rating}
+            seconds={60}
+            symbol={player.symbol}
+            isRight={index % 2 === 1}
+          />
+        ))}
+        gameMoveInfo={
+          <GameMoveInfo currentMove={currentMove} nextMove={nextMove} />
+        }
+        gameCells={cells.map((cell, index) => (
+          <GameCell
+            key={index}
+            isWinner={winnerSequence?.includes(index)}
+            disabled={!!winnerSymbol}
+            onClick={() => {
+              handleCellClick(index);
+            }}
+            symbol={cell}
+          />
+        ))}
+      />
+      <GameOverModal
+        winnerName={winnerPlayer?.name}
+        players={PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => (
+          <PlayerInfo
+            key={player.id}
+            avatar={player.avatar}
+            name={player.name}
+            rating={player.rating}
+            seconds={60}
+            symbol={player.symbol}
+            isRight={index % 2 === 1}
+          />
+        ))}
+      />
+    </>
   );
 }
