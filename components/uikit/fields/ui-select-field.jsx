8728d152b3c98d84ca5ff109b06d38963797f33a
Ivan <99608223+ivan1945kozhevnikov@users.noreply.github.com>
2024-11-09 12:46:18 +0300

    Добавил коипонент UiSelect

--- components/uikit/fields/ui-select-field.jsx
+++ components/uikit/fields/ui-select-field.jsx
@@ -1,5 +1,6 @@
 import { UiFieldLabel } from "./ui-field-label";
 import { UiFieldMessage } from "./ui-field-message";
+import { UiSelect } from "./ui-select";
 
 /**
  * @param {Object} props
@@ -23,6 +24,7 @@ export function UiSelectField({
   return (
     <div className={className}>
       <UiFieldLabel label={label} required={required} />
+      <UiSelect className="mt-1" options={options} />
       <UiFieldMessage helperText={helperText} errorText={errorText} />
     </div>
   );
